8a46003d6100658227ed0c11a2d6c7bb16066c34
Christopher Comella <git.chrisco@gmail.com>
2016-05-18 11:58:22 -0600

    Populate select boxes from JSON file with jQuery Right now I'm entering venue data manually in Google Sheets. And converting it to JSON with this plug-in: https://chrome.google.com/webstore/detail/export-sheet-data/bfdcopkbamihhchdnjghdknibmcnfplk Danny suggested this to get data directly from Google Sheets: https://www.npmjs.com/package/tabletop

--- public/app.js
+++ public/app.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
-  console.log("jQuery is loaded");
+	console.log("jQuery is loaded");
+	$.get("venues.json").then(createOptions);
+
+	function createOptions(rawData) {
+		rawData.Sheet1.forEach(function(venue) {
+			console.log(venue);
+			var $option = $("<option>")
+				.text(venue.venue)
+				.attr("value", venue.address);
+      var $option2 = $option.clone();
+      $("#start").append($option);
+      $("#end").append($option2);
+		});
+	}
 });
