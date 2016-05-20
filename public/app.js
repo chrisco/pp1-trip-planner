f56d0ec4700757cd43edc0319785db5b8fb4b830
Christopher Comella <git.chrisco@gmail.com>
2016-05-20 06:46:46 -0600

    Add info marker to map. Show duration, distance. Also change tabs to spaces on all files for legibility and consistency. NOTE TO SELF: Change Atom preferences?

--- public/app.js
+++ public/app.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
-	// $.get("meetups.json").then(createOptions);
+  // $.get("meetups.json").then(createOptions);
 
-	function createOptions(data) {
-		data.forEach(function(meetup) {
-			var $option = $("<option>")
-				.text(meetup.name + " - " + meetup.venue)
-				.attr("value", meetup.address);
-			var $option2 = $option.clone();
-			$("#start").append($option);
-			$("#end").append($option2);
-		});
-	}
+  function createOptions(data) {
+    data.forEach(function(meetup) {
+      var $option = $("<option>")
+        .text(meetup.name + " - " + meetup.venue)
+        .attr("value", meetup.address);
+      var $option2 = $option.clone();
+      $("#start").append($option);
+      $("#end").append($option2);
+    });
+  }
 
-	init(function(data) {
-		createOptions(data);
-	});
+  init(function(data) {
+    createOptions(data);
+  });
 });
--- public/tabletop.js
+++ public/tabletop.js
@@ -1,8 +1,8 @@
 function init(callback) {
-	Tabletop.init({
-		key: '1MmZROtcJNQnDB1Oi2CDSrSFqNNV5h-Xp0_LsgLMGj3k',
-		callback: callback,
-			// console.log(data);,
-		simpleSheet: true
-	});
+  Tabletop.init({
+    key: '1MmZROtcJNQnDB1Oi2CDSrSFqNNV5h-Xp0_LsgLMGj3k',
+    callback: callback,
+      // console.log(data);,
+    simpleSheet: true
+  });
 }
