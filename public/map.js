7fc912aa85cca787d6eae2e269c74310b1b9a2c8
Christopher Comella <git.chrisco@gmail.com>
2017-03-17 21:15:07 -0400

    Beautify

--- public/map.js
+++ public/map.js
@@ -6,10 +6,10 @@ function initMap() {
   var duration; // And function at the bottom of the file
 
   // Instantiate a directions service.
-  var directionsService = new google.maps.DirectionsService;
+  var directionsService = new google.maps.DirectionsService();
 
   // Create a map and center it on Manhattan.
-  var map = new google.maps.Map(document.getElementById('map'), {
+  var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 13,
     // center: {lat: 40.771, lng: -73.974} // Manhattan
     center: {
@@ -24,26 +24,41 @@ function initMap() {
   });
 
   infowindow = new google.maps.InfoWindow({
-    content: ''
-  })
+    content: ""
+  });
 
   // Instantiate an info window to hold step text.
-  var stepDisplay = new google.maps.InfoWindow;
+  var stepDisplay = new google.maps.InfoWindow();
 
   // Display the route between the initial start and end selections.
   calculateAndDisplayRoute(
-    directionsDisplay, directionsService, markerArray, stepDisplay, map);
+    directionsDisplay,
+    directionsService,
+    markerArray,
+    stepDisplay,
+    map
+  );
   // Listen to change events from the start and end lists.
-  var onChangeHandler = function() {
+  var onChangeHandler = function () {
     calculateAndDisplayRoute(
-      directionsDisplay, directionsService, markerArray, stepDisplay, map);
+      directionsDisplay,
+      directionsService,
+      markerArray,
+      stepDisplay,
+      map
+    );
   };
-  document.getElementById('start').addEventListener('change', onChangeHandler);
-  document.getElementById('end').addEventListener('change', onChangeHandler);
+  document.getElementById("start").addEventListener("change", onChangeHandler);
+  document.getElementById("end").addEventListener("change", onChangeHandler);
 }
 
-function calculateAndDisplayRoute(directionsDisplay, directionsService,
-  markerArray, stepDisplay, map) {
+function calculateAndDisplayRoute(
+  directionsDisplay,
+  directionsService,
+  markerArray,
+  stepDisplay,
+  map
+) {
   // First, remove any existing markers from the map.
   for (var i = 0; i < markerArray.length; i++) {
     markerArray[i].setMap(null);
@@ -58,29 +73,34 @@ function calculateAndDisplayRoute(directionsDisplay, directionsService,
   // Retrieve the start and end locations and create a DirectionsRequest using
   // WALKING directions.
   directionsService.route({
-    origin: document.getElementById('start').value,
-    destination: document.getElementById('end').value,
-    travelMode: google.maps.TravelMode.WALKING
-  }, function(response, status) {
-    // Route the directions and pass the response to a function to create
-    // markers for each step.
-    if (status === google.maps.DirectionsStatus.OK) {
-      document.getElementById('warnings-panel').innerHTML =
-        '<b>' + response.routes[0].warnings + '</b>';
-      directionsDisplay.setDirections(response);
+      origin: document.getElementById("start").value,
+      destination: document.getElementById("end").value,
+      travelMode: google.maps.TravelMode.WALKING
+    },
+    function (response, status) {
+      // Route the directions and pass the response to a function to create
+      // markers for each step.
+      if (status === google.maps.DirectionsStatus.OK) {
+        document.getElementById("warnings-panel").innerHTML = "<b>" +
+          response.routes[0].warnings +
+          "</b>";
+        directionsDisplay.setDirections(response);
 
-      // Get distance and duration.
-      distance = directionsDisplay.directions.routes[0].legs[0].distance.text;
-      // console.log(distance);
-      duration = directionsDisplay.directions.routes[0].legs[0].duration.text;
-      // console.log(duration);
-      // Update the distance and duration.
-      infowindow.setContent('Distance:' + distance + '<br/>Duration:' + duration);
-      showSteps(response, markerArray, stepDisplay, map);
-    } else {
-      window.alert('Directions request failed due to ' + status);
+        // Get distance and duration.
+        distance = directionsDisplay.directions.routes[0].legs[0].distance.text;
+        // console.log(distance);
+        duration = directionsDisplay.directions.routes[0].legs[0].duration.text;
+        // console.log(duration);
+        // Update the distance and duration.
+        infowindow.setContent(
+          "Distance:" + distance + "<br/>Duration:" + duration
+        );
+        showSteps(response, markerArray, stepDisplay, map);
+      } else {
+        window.alert("Directions request failed due to " + status);
+      }
     }
-  });
+  );
 }
 
 function showSteps(directionResult, markerArray, stepDisplay, map) {
@@ -93,11 +113,15 @@ function showSteps(directionResult, markerArray, stepDisplay, map) {
   var midIndex = Math.floor(myRoute.steps.length / 2);
 
   for (var i = 0; i < myRoute.steps.length; i++) {
-    var marker = markerArray[i] = markerArray[i] || new google.maps.Marker;
+    var marker = (markerArray[i] = markerArray[i] || new google.maps.Marker());
     marker.setMap(map);
     marker.setPosition(myRoute.steps[i].start_location);
     attachInstructionText(
-      stepDisplay, marker, myRoute.steps[i].instructions, map);
+      stepDisplay,
+      marker,
+      myRoute.steps[i].instructions,
+      map
+    );
 
     // For one of the markers, display the route info.
     if (i == midIndex) {
@@ -107,7 +131,7 @@ function showSteps(directionResult, markerArray, stepDisplay, map) {
 }
 
 function attachInstructionText(stepDisplay, marker, text, map) {
-  google.maps.event.addListener(marker, 'click', function() {
+  google.maps.event.addListener(marker, "click", function () {
     // Open an info window when the marker is clicked on, containing the text
     // of the step.
     stepDisplay.setContent(text);
--- public/tabletop.js
+++ public/tabletop.js
@@ -1,8 +1,7 @@
 function init(callback) {
   Tabletop.init({
     key: '1MmZROtcJNQnDB1Oi2CDSrSFqNNV5h-Xp0_LsgLMGj3k',
-    callback: callback,
-      // console.log(data);,
-    simpleSheet: true
+    callback,
+    simpleSheet: true,
   });
 }
